5b934fdb0c334f22f4eb6014680a3dc1593573e2
tunyakin <tvlinkor@mail.ru>
2025-02-13 16:49:33 +0300

    Appended user fields

--- db/migrate/20250212211944_create_users.rb
+++ db/migrate/20250212211944_create_users.rb
@@ -2,7 +2,9 @@ class CreateUsers < ActiveRecord::Migration[8.0]
   def change
     create_table :users do |t|
       t.string :name, null: false
+      t.string :surname, null: false
       t.string :patronymic, null: true
+      t.string :full_name, null: false
       t.string :email, null: false
       t.integer :age, null: false
       t.string :nationality, null: true
--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_12_212228) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
+    t.string "surname", null: false
     t.string "patronymic"
+    t.string "full_name", null: false
     t.string "email", null: false
     t.integer "age", null: false
     t.string "nationality"
